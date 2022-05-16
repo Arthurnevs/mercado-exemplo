05f9ded11a477cd36b7a38fb859d6136b5e36a9e
Arthurnevs <jose.arthur.brito@ccc.ufcg.edu.br>
2022-05-16 09:21:28 -0300

    commit

---
+++ src/exemplo/LoteRepository.java
@@ -0,0 +1,13 @@
+package exemplo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoteRepository {
+
+	private Map<String,Produto> lotes = new HashMap<String,Produto>();
+	
+
+
+	
+}
